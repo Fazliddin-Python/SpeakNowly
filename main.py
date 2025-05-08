81dda651c3d75a9c8368b42db9d238411052900d
fazliddindehranov <fazliddindehranov@gitlab.com>
2025-05-08 20:25:56 +0500

    Main URL changed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from api.client_site.v1.routes import router as client_site_v1_router
 
 app = FastAPI()
 
-app.include_router(client_site_v1_router, prefix="/api/client_site/v1", tags=["Client Site v1"])
+app.include_router(client_site_v1_router, prefix="/v1", tags=["Client Site v1"])
 # app.include_router(dashboard_v1_router, prefix="/api/dashboard/v1", tags=["Dashboard v1"])
 # app.include_router(mobile_v1_router, prefix="/api/mobile/v1", tags=["Mobile v1"])
 
