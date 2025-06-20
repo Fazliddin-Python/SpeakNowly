03e4fb5f3c53715e969198f8c438b61fd5bbcfe6
fazliddindehranov <fazliddindehranov@gitlab.com>
2025-06-20 14:49:23 +0500

    seed fix

--- seed_fixtures.sh
+++ seed_fixtures.sh
@@ -4,35 +4,35 @@ set -e
 DB_URL="postgresql://backender:Fazliddin.000@localhost:5432/speaknowly"
 
 for file in fixtures/*.json; do
-    table=$(basename "$file" .json)
-    echo "→ Seeding (upsert) data into table: $table"
+  table=$(basename "$file" .json)
+  echo "→ Сеем (upsert) данные в таблицу: $table"
 
-    jq -c '.[]' "$file" | while read -r row; do
-        # columns
-        readarray -td, cols_array < <(echo "$row" | jq -r 'keys_unsorted | @csv' | sed 's/"//g,' )
-        unset 'cols_array[-1]' # remove empty element from readarray
+  jq -c '.[]' "$file" | while read -r row; do
+    # колонки
+    readarray -td, cols_array < <(echo "$row" | jq -r 'keys_unsorted | @csv' | sed 's/"//g,' )
+    unset 'cols_array[-1]' # убрать пустой элемент из readarray
 
-        # values
-        vals=$(echo "$row" | jq -r '[.[]] |
-            map(
-                if type=="string" then
-                    ("'"'"'" + gsub("'"'"'" ; "''") + "'"'"'")
-                elif . == null then
-                    "NULL"
-                else
-                    tostring
-                end
-            ) | @csv')
-        vals=${vals//\"/}
+    # значения
+    vals=$(echo "$row" | jq -r '[.[]] |
+      map(
+        if type=="string" then
+          ("'"'"'" + gsub("'"'"'" ; "''") + "'"'"'")
+        elif . == null then
+          "NULL"
+        else
+          tostring
+        end
+      ) | @csv')
+    vals=${vals//\"/}
 
-        cols=$(IFS=, ; echo "${cols_array[*]}")
-        update_clause=$(printf ", %s = EXCLUDED.%s" "${cols_array[@]:1}" "${cols_array[@]:1}")
-        update_clause=${update_clause:2}
+    cols=$(IFS=, ; echo "${cols_array[*]}")
+    update_clause=$(printf ", %s = EXCLUDED.%s" "${cols_array[@]:1}" "${cols_array[@]:1}")
+    update_clause=${update_clause:2}
 
-        psql "$DB_URL" -c \
-            "INSERT INTO $table ($cols) VALUES ($vals)
-             ON CONFLICT (id) DO UPDATE SET $update_clause;"
-    done
+    psql "$DB_URL" -c \
+      "INSERT INTO $table ($cols) VALUES ($vals)
+       ON CONFLICT (id) DO UPDATE SET $update_clause;"
+  done
 done
 
-echo "✅ All fixtures applied (upsert)."
+echo "✅ Все fixtures применены (upsert)."
