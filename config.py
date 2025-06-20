6e91456f09f8550190d6b32bbdd86ecc87d0d67e
fazliddindehranov <fazliddindehranov@gitlab.com>
2025-06-20 10:10:36 +0500

    database

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 # === Database (Tortoise ORM) ===
-DATABASE_URL = "postgres://SpeakNowly_owner:npg_0VE3cUXbhdRn@ep-square-credit-a2fs6dxa-pooler.eu-central-1.aws.neon.tech/SpeakNowly"
+DATABASE_URL = config("DATABASE_URL")
 DATABASE_CONFIG = {
     "connections": {"default": DATABASE_URL},
     "apps": {
